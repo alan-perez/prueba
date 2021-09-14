feb9ed013f6b4ad8303d43773b17bab5c6a7efdf
alan-perez <alanperez.inbox@gmail.com>
2021-09-13 19:43:21 -0700

    commit prueba

---
+++ primer-proyecto/src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <h1>Test Prototyping</h1>
+
+<h3>{{this.nombre}}</h3>
+</div>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        nombre: "Alan",
+    }
+
+
+}
+}
+</script>
+
+<style>
+
+</style>
